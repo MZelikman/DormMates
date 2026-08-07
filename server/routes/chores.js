46d8f7d80137ba227e7a20a33a449b41c81c7132
MZelikman <zelikmanmaxim@gmail.com>
2026-08-06 20:46:46 -0400

    routes

---
+++ server/routes/chores.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { Chore } from '../models/Chore.js'
+
+const express = require('express');
+const router = express.Router();
+const model = require('../models/Chore');
+
