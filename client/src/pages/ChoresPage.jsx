86faf66ac1b36347aff1a6df675d5b82ed7a45e3
MaxZelik <maximzelikman0@gmail.com>
2026-08-11 06:07:29 -0400

    pages

--- client/src/pages/ChoresPage.jsx
+++ client/src/pages/ChoresPage.jsx
@@ -1,5 +1,49 @@
+import { useState, useEffect } from 'react';
+import { getChores, addChore, completeChore, deleteChore, getRoommates } from '../api.js';
+
+
 function ChoresPage() {
-  return <h1>Chores (coming soon)</h1>;
+  const [chores, setChores] = useState([]);
+  const [roommates, setRoommates] = useState([]);
+  const [choreName, setChoreName] = useState('');
+  const [frequencyDays, setFrequencyDays] = useState(7);
+  const [selectedRoommates, setSelectedRoommates] = useState([]);
+
+  const loadChores = async () => {
+    const response = await getChores();
+    setChores(response.data);
+  }
+
+  
+  const loadRoommates = async () => {
+    const response = await getRoommates();
+    setRoommates(response.data);
+    setSelectedRoommates(response.data.map((roommate) => roommate._id))
+  }
+
+  useEffect(() => {
+    loadRoommates();
+    loadChores();
+  }, []);
+
+  const toggleRoommate = (id) => {
+    if (selectedRoommates.includes(id)) {
+      setSelectedRoommates(selectedRoommates.filter((roommate) => roommate !== id));
+    } else {
+      
+      setSelectedRoommates([...selectedRoommates, id]);
+
+    }
+  }
+
+
+
+
+  return (
+    <div>
+      <h1>Chores</h1>
+    </div>
+  );
 }
 
 export default ChoresPage;
